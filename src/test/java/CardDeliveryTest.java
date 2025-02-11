564b546b1fb456b51d0291049a0af491a7460100
Anna Paliy <annpaliy2015@gmail.com>
2025-02-11 13:36:23 +0300

    Initial commit

---
+++ src/test/java/CardDeliveryTest.java
@@ -0,0 +1,9 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class CardDeliveryTest {
+}
